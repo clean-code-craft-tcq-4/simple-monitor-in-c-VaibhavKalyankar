d2a144717fe21afff2a69134bf4db5e85b853af8
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-07-09 20:03:04 +0530

    Create Battery_Checker.h

---
+++ Battery_Checker.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#define Not_Ok 1
+#define Ok 0
+#define Min_Temp 0
+#define Max_Temp 45
+#define Min_Soc 20
+#define Max_Soc 80
+#define ChargeRate 0.8
+
+int IsbatteryOk(float,float, float); 
+int IsbatteryTempCheck(float);
+int IsbatterySocCheck(float);
+int IsbatteryChargeRateCheck(float);
