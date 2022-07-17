8f7dd73468544a496460de35c68a4acc06e25e35
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-07-17 17:46:16 +0530

    Update Check_HighBreach.c

--- Check_HighBreach.c
+++ Check_HighBreach.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<Battery_Checker.h>
 
+
+extern BatteryStatus_tst BatteryStatus_st;
+
+void CheckTempforHighBreach()
+{
+  BatteryStatus_st.TempStatus = HIGH_TEMP_BREACH;
+  printf("\nHigh Temprature Breach\n");
+}
+
+void CheckSocforHighBreach()
+{
+  BatteryStatus_st.SocStatus = HIGH_SOC_BREACH;
+  printf("\nHigh Soc Breach\n");
+}
+
+void CheckChargeRateforHighBreach()
+{
+  BatteryStatus_st.ChargeRateStatus = HIGH_ChargeRate_BREACH;
+  printf("\nHigh ChargeRate Breach\n");
+}
