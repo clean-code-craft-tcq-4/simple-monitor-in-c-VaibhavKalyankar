5269c421b05315447223f35979cd7be144b05033
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-07-17 17:42:23 +0530

    Update Check_LowBreach.c

--- Check_LowBreach.c
+++ Check_LowBreach.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<Battery_Checker.h
 
+extern BatteryStatus_tst BatteryStatus_st
+
+CheckTempforLowBreach()
+{
+  BatteryStatus_st.TempStatus = LOW_TEMP_BREACH;
+  printf("\nLow Temprature Breach\n");
+}
+
+CheckSocforLowBreach()
+{
+  BatteryStatus_st.SocStatus = LOW_SOC_BREACH;
+  printf("\nLow Soc Breach\n");
+}
+
+CheckChargeRateforLowBreach()
+{
+  BatteryStatus_st.ChargeRateStatus = LOW_ChargeRate_BREACH;
+  printf("\nLow ChargeRate Breach\n");
+}
